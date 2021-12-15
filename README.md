4f160d4040d16e5a73629973ca26bd22f61bf4f4
Pontus1337 <lindhpontus1@gmail.com>
2021-12-15 16:38:37 +0100

    I have finished the program so that it now works

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Substitute-santa
+
+This is a program for writing your wishlists, after you have read or wrote your wishlish you can make a nughtylist.
+
+## Features
+
+Writing a wishlist, reading a wishlist.
+Reading the naughtylist, adding to the naughtylist.
--- substitutesanta.py
+++ substitutesanta.py
@@ -1,6 +1,16 @@
 # Substitute santa
 
+#naughtylist
+def rn():
+    with open("naughtylist.txt","r",encoding="utf8") as f:
+        for i in f.readlines():
+            print(i.strip("\n"))
 
+def rr():    
+    with open("naughtylist.txt","a",encoding="utf8") as f:
+        kol=input("who has been naugty?\n")
+        print("These are alredy in the list")
+        f.writelines(f"\n{kol}")
 
 def main():
     while True:
@@ -16,20 +26,36 @@ def main():
         wish=""
         ad=input("What do you wnat your wishlist to be called\n").lower()
         nam=input("What is your name?")
+        with open(f"{ad}.txt","w", encoding="utf8") as f:
+            f.write(f"n{nam}\n")
         while True:
             wish=input("What do you wish for christmas?, press enter to go out of this mode\n")
             if wish=="":
                 break
             else:
-                with open(f"{ad}.txt","w", encoding="utf8") as f:
-                    f.write(f"{wish}\n")
-        with open(f"{ad}.txt","w", encoding="utf8") as f:
-            f.write(f"\n{nam}")
+                with open(f"{ad}.txt","a", encoding="utf8") as f:
+                    f.write(f"\n{wish}")
+
     elif "read" in a:
         ad=""
         ad=input("What is the wishlist that you want to read called?\n").lower()
         with open(f"{ad}.txt","r", encoding="utf8") as f:
             print(f.read())
+
+
+    
+    val=input("read or write naughty list\n")
+    if "read" in val:
+        rn()
+    elif "write" in val:
+        rn()
+        rr()
+
+
+
+
+
+
 
 
 if __name__=="__main__":
